b42a580310c705e9ed4fceecd50c89d883ef2f65
Parkerr995 <95656036+Parkerr995@users.noreply.github.com>
2021-12-06 16:25:57 -0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+	$('.category_list .category-item[category="all"]').addClass('ct_item-active');
+	
+	$('.category-item').click(function(){
+
+		var catProduct = $(this).attr('category');
+		
+		$('.category-item').removeClass('ct_item-active');
+		$(this).addClass('ct_item-active');
+
+
+			$('.product-item').hide();
+			$('.product-item[category="'+catProduct+'"]').show();
+
+
+	});
+
+		$('.category-item[category="all"]').click(function() {
+			$('.product-item').show();
+	});
+
+});
